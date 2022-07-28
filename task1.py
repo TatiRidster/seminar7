7a238a716c2c3573d955135af0f06d442a1c031a
Tatiana <tatnovid@gmail.com>
2022-07-28 21:56:26 +0300

    рациональные числа

--- task1.py
+++ task1.py
@@ -1,3 +1,34 @@
-n = 5
-rez = [(-3)**x for x in range(0,n)]
-print(rez)
+from fractions import Fraction
+def rational_number():
+    lst = list(map(int, input("Введите 4 числа через пробел:\n").split()))
+    a=lst[0] 
+    b = lst[1]
+    c = lst[2]
+    d = lst[3]
+    a = Fraction(a, b) # a =  - рациональное число
+    b = Fraction(c, d) # b =  - рациональное число
+    print(a,b)
+    return a,b
+def sum_rational_num(a,b,c,d):
+    sum_num = Fraction(a, b) + Fraction(c, d) 
+    return sum_num
+def minus_rational_num(a,b,c,d):    
+    minus_num = Fraction(a, b) - Fraction(c, d)  
+    return minus_num
+def multiply_rational_num(a,b,c,d):
+    mult= Fraction(a, b) * Fraction(c, d)
+    return mult
+def division_rational_num(a,b,c,d):  
+    divis = Fraction(a, b) / Fraction(c, d)
+    return divis
+
+#rational_number()
+lst = list(map(int, input("Введите 4 числа через пробел:\n").split()))
+a =lst[0] 
+b = lst[1]
+c = lst[2]
+d = lst[3]
+print(sum_rational_num(a,b,c,d))    
+print(minus_rational_num(a,b,c,d))
+print(multiply_rational_num(a,b,c,d))
+print(division_rational_num(a,b,c,d))
